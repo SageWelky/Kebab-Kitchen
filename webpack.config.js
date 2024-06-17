f384745890c1f4be587c8e467107d5ad0a447fc8
Sage Welky <ehrenwelky@gmail.com>
2024-06-17 13:11:15 -0500

    Set up basic style and template for site. Ready for creation of individual html tab creation.

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
    },
  };
